bf3a310d0edc98831539053f6beb563912b7b491
Patrick Quinlivan <pjq002@bucknell.edu>
2022-10-13 23:50:06 -0400

    "DictionaryProcessorTest Finished"

--- src/test/java/wordlegroup/DictionaryProcessorTest.java
+++ src/test/java/wordlegroup/DictionaryProcessorTest.java
@@ -7,9 +7,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryProcessorTest {
+    /**
+     * Initilize a DictionaryProcessor
+     */
+    private DictionaryProcessor processor;
 
     @BeforeEach
     void setup(){
+        //set up processor before test
+        processor = new DictionaryProcessor("url");
 
     }
 
@@ -20,6 +26,26 @@ class DictionaryProcessorTest {
 
     @Test
     void wordProcessorTest(){
+        //Make sure setOfWords is empty to start
+        assertEquals(0,processor.getSetOfWords().size());
+
+        //Make sure it adds HELLO
+        processor.processWord("HELLO");
+        assertEquals(1,processor.getSetOfWords().size());
+
+        //make sure it does not add words that are too short
+        processor.processWord("HI");
+        assertEquals(1,processor.getSetOfWords().size());
+
+        //Make sure it does not add words that are too long
+        processor.processWord("YELLOW");
+        assertEquals(1,processor.getSetOfWords().size());
+
+        //Make sure it only adds words that are upper case
+        processor.processWord("hello");
+        assertEquals(1,processor.getSetOfWords().size());
+
+
 
     }
 }
