07d9c9325b8be76b06b5678606b3d6a5434b671b
Patrick Quinlivan <pjq002@bucknell.edu>
2022-10-13 23:42:02 -0400

    "DictionaryProcessorTest set up"

---
+++ src/test/java/wordlegroup/DictionaryProcessorTest.java
@@ -0,0 +1,25 @@
+package wordlegroup;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryProcessorTest {
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+    @AfterEach
+    void teardown(){
+
+    }
+
+    @Test
+    void wordProcessorTest(){
+
+    }
+}
